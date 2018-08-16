2c20fe0bb418874ab60d75e95f5358ad3933d807
Thomas Athanasiou <athanthomas777@gmail.com>
2018-08-16 16:21:15 +0300

    Testing calendar events

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from events.models import Event
 from events.views import get_all_events, getHomePage
 
 urlpatterns= [
-	url(r'^$', getHomePage),
-	url(r'^get-all-events', get_all_events),
-	url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Event, template_name='events/index.html'))]
+	#url(r'^$', getHomePage),
+	#url(r'^get-all-events', get_all_events),
+	url(r'^$', ListView.as_view(queryset=Event.objects.all().order_by("id")[:10],template_name="events/event.html")),
+	url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Event, template_name='events/calendar.html'))]
